a8830fdbac6cee625e3893e050771deb8ab69ded
Hitarth-Rami-22 <hitarth.rami@bacancy.com>
2025-03-10 10:03:46 +0530

    ef_core_setup

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -1,6 +1,12 @@
 ﻿namespace EF_Task_1
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+          public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // Add your DbSet properties here (Example)
+        public DbSet<Student> Students { get; set; }
     }
 }
