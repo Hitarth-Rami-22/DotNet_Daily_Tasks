0fb7b26a4fae495fac2561361ea3e19dd23a6cdd
Hitarth-Rami-22 <hitarth.rami@bacancy.com>
2025-03-10 10:13:10 +0530

    onconfiguring_setup

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -1,10 +1,19 @@
-﻿namespace EF_Task_1
+﻿using Microsoft.EntityFrameworkCore;
+namespace EF_Task_1
 {
     public class ApplicationDbContext : AddDbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+          protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=YOUR_SERVER;Database=EFCoreDB;Trusted_Connection=True;TrustServerCertificate=True;");
+            }
+        }
+        // public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    // Define DbSets (tables)
-    public DbSet<Product> Products { get; set; }
+         // Define DbSets (tables)
+        public DbSet<Product> Products { get; set; }
+    }
+
 }
-
