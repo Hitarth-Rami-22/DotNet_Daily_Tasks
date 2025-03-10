8baa6f2650bafd2d49a055dcd1a0b7af24533240
Hitarth-Rami-22 <hitarth.rami@bacancy.com>
2025-03-10 10:25:54 +0530

    environment-based connection

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -1,6 +1,25 @@
-﻿namespace EF_Task_1
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace EF_Task_1.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+      
+        public DbSet<Student> Students { get; set; } // Changed from Products to Students
+    }
+
+    // Rename the class from Students to Student
+    public class Student
+    {
+        public int Id { get; set; } // Primary Key
+
+        public string FirstName { get; set; } // First Name of the Student
+
+        public string Email { get; set; } // Email Address of the Student
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
+using EnvironmentBasedApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<ApplicationDbContext>();
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Get the current environment
+var environment = builder.Environment.EnvironmentName;
 
 var app = builder.Build();
 
