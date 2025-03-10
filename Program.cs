3aaa151e68ebf51517f852762ccb54eaca17c50c
Hitarth-Rami-22 <hitarth.rami@bacancy.com>
2025-03-10 10:31:37 +0530

    dbcontext_scoped

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.Extensions.DependencyInjection;
+using EF_Task_1.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")),
+    ServiceLifetime.Scoped);
+
 
 // Add services to the container.
 
